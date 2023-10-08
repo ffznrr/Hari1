50c90fb7b9dbb293c7769c07b09865af188e4300
ffznrr <ffznrr@gmail.com>
2023-10-08 18:34:06 +0700

    day-5

--- js/myproject.js
+++ js/myproject.js
@@ -62,6 +62,26 @@ function addData(event) {
     let description = document.getElementById("input-description").value;
     let images = document.getElementById("input-blog-image").files[0]; // Mengambil file pertama
 
+
+
+    selisih(startdate, enddate);
+
+
+    if(projectname === ""){
+        alert("Nama project tidak boleh kosong");
+        return;
+    }else if(startdate === "" || enddate === ""){
+        alert("Isi Tanggal Mulainya dan Akhir Projek Terlebih Dahulu");
+        return;
+    }else if( description === ""){
+        alert("Isi Deskripsi Anda terlebih dahulu")
+        return;
+    }else if( images === 0){
+        alert("Isi Gambar Anda terlebih dahulu")
+        return;
+    }
+
+
     let blog = {
         projectname: projectname,
         startdate: startdate,
@@ -69,6 +89,7 @@ function addData(event) {
         description: description,
         checkboxes: {}, // Inisialisasi objek untuk menyimpan nilai checkbox
         images: URL.createObjectURL(images), // Mengubah file input menjadi URL
+        duration: selisih(startdate, enddate), 
     };
 
     let checkboxes = document.querySelectorAll(".checkboxes");
@@ -83,6 +104,7 @@ function addData(event) {
     console.log(dataBlog);
 
     renderBlog();
+    selisih( startdate, enddate);
 }
 
 function renderBlog() {
@@ -102,7 +124,8 @@ function renderBlog() {
         <div class="card" id="contents">  
             <img class="project-pict" src="${dataBlog[i].images}" >
             <h3><a href="detail-project.html">${dataBlog[i].projectname}</a></h3>
-            <p>durasi: 3 bulan</p>
+            <p>${dataBlog[i].duration}</p>
+
 
             <p style="text-align: justify; margin-top: 5px;">${dataBlog[i].description}</p>
             <div class="framework-logo">
@@ -117,11 +140,78 @@ function renderBlog() {
         </div>
         
        
-        
 </div>
         `;
     };
 };
 
 
+function selisih(startdate, enddate) {
+    let date1 = new Date(startdate); 
+    let date2 = new Date(enddate); 
+      
+    let Difference_In_Time = date2.getTime() - date1.getTime(); 
+      
+  
+    let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24); 
+
+    let hasal = Difference_In_Days;
     
+    let hasil = `Durasi : ${hasal} hari`;
+    return hasil; 
+}
+
+// function getFullTime(){
+//     let time = new Date();
+//     // console.log(time);
+
+//     let monthName = [
+//         "January",
+//         "February",
+//         "March",
+//         "April",
+//         "May",
+//         "June",
+//         "July",
+//         "August",
+//         "September",
+//         "October",
+//         "November",
+//         "December"
+//     ];
+//     // console.log(monthName[2]);
+
+//     let daying = [
+//         "Sunday",
+//         "Monday",
+//         "Tuesday",
+//         "Wednesday",
+//         "Thursday",
+//         "Friday",
+//         "Saturday",
+//     ];
+
+//     // console.log(daying[0]);
+
+//     let day = time.getDay();
+//     // console.log(day);
+
+//     let hari = daying[day];
+//     // console.log(hari);
+
+   
+   
+
+//     let date = time.getDate();
+//     // console.log(date);
+
+//     let monthIndex = time.getMonth();
+//     // console.log(monthIndex +1);
+
+//     let bulanInih = monthName[monthIndex];
+//     // console.log(bulanInih);
+
+
+
+// }
+
